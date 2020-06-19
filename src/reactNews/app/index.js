b6bfdac3451a161250396e0317ac6e1c847d6030
Elise Weinfurter <elise.weinfurter@ibm.com>
2020-06-19 10:10:21 -0500

    Review forEach

--- src/reactNews/app/index.js
+++ src/reactNews/app/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Axios from 'axios';
 
-/*class Sad extends Component {
+/*class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,7 +18,7 @@ import Axios from 'axios';
       console.log('articles', json);
       let { articles } = json;
       this.setState({articles});
-    })
+    });
   }
 
   render() {
@@ -35,7 +35,8 @@ function loadAPI() {
     method: 'get',
     url: 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-05-19&sortBy=publishedAt&apiKey=92373e995c97437a974ef496fabf8ab1',
     params: {
-      language: 'en'
+      language: 'en',
+      sortBy: 'relevancy'
     }
   })
     .then(response => iterateArt(response))
@@ -43,7 +44,31 @@ function loadAPI() {
 }
 
 function iterateArt(response) {
-  const responseArr = response.data.articles;
+  const res = response.data.articles;
+
+  res.forEach(arr => {
+    return document.getElementById('root').innerHTML = `
+    <div style="background-color:  #f3852a;color:black;width:300px;height:500px" class="limit">
+      <a href={arr.url}>
+        <img
+          ng-src={arr.urlToImage}
+          alt="image not avaliable"
+          style="width:300px;height:400px;"
+          position="center"
+        />
+      </a>
+      <h3 class="text-center">${arr.title}</h3>
+      <p class="display-9 text-center">
+        ${arr.author} <br><br>
+        <h7 class="text-center"style="bold">Published: ${arr.publishedAt} <br></h7>
+        Description: ${arr.description} <br>
+        <a href={arr.url}>link</a>
+        </p>
+    </div>
+    `;
+  });
+
+  /*const responseArr = response.data.articles;
   //Add console.log(arr) below to show the map is working.
   const resArray = responseArr.map(arr => {
     return document.getElementById('root').innerHTML = `
@@ -65,7 +90,7 @@ function iterateArt(response) {
         </p>
     </div>
     `;
-  });
+  });*/
 }
 
 loadAPI();
